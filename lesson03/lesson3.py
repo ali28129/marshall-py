9a4ae6c9510f62c203b6cfafb6d24418697b0308
Ali <alianamio@outlook.com>
2025-02-14 14:01:22 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,15 @@
 # Lesson 3
+# Maximum square side length based on input
+
+# input
+tiles = input("Enter the number of tiles: ")
+# input() always a string
+tiles = int(tiles)
+
+# square root is exponent of a half
+calculation = int((tiles ** 0.5) // 1)
+# can also import math module, import math; calculations = math.sqrt(tiles)
+
+print(f"The maximum side length is {calculation}")
+
+.
